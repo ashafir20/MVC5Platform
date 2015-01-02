8e06a7fc0c9db2e9ce76cb78627a85d2a3486aac
ashafir <aviv@optimalfinance.co.il>
2015-01-02 23:35:01 +0200

    added bootstrap styling

---
+++ MVC5Platform/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using MVC5Platform.Models;
+
+namespace MVC5Platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+            if (oldColor != null)
+            {
+                Votes.ChangeVote(color, (Color)oldColor);
+            }
+            else
+            {
+                Votes.RecordVote(color);
+            }
+            ViewBag.SelectedColor = Session["color"] = color;
+            return View();
+        }
+    }
+}
