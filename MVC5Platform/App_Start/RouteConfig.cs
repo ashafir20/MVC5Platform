3632df1a40500903fb5a53ad75542969153ed416
Aviv Shafir <aviv@optimalfinance.co.il>
2015-01-21 22:00:06 +0200

    started with handlers ch 5

--- MVC5Platform/App_Start/RouteConfig.cs
+++ MVC5Platform/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MVC5Platform.Infrastructure;
 
 namespace MVC5Platform
 {
@@ -13,11 +14,22 @@ namespace MVC5Platform
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.Add(new Route("handler/{*path}", new CustomRouteHandler { HandlerType = typeof(DayOfWeekHandler) }));
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+        }
+
+        class CustomRouteHandler : IRouteHandler
+        {
+            public Type HandlerType { get; set; }
+            public IHttpHandler GetHttpHandler(RequestContext requestContext)
+            {
+                return (IHttpHandler)Activator.CreateInstance(HandlerType);
+            }
         }
     }
 }
---
+++ MVC5Platform/Infrastructure/DayOfWeekHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+
+namespace MVC5Platform.Infrastructure
+{
+    public class DayOfWeekHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string day = DateTime.Now.DayOfWeek.ToString();
+            if (context.Request.CurrentExecutionFilePathExtension == ".json")
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.Write(string.Format("{{\"day\": \"{0}\"}}", day));
+            }
+            else
+            {
+                context.Response.ContentType = "text/html";
+                context.Response.Write(string.Format("<span>It is: {0}</span>", day));
+            }
+        }
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
