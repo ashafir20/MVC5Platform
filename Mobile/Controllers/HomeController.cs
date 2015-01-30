fa51aea20430e75b2de931220e5903b34d6f2418
Aviv Shafir <aviv@optimalfinance.co.il>
2015-01-30 10:54:51 +0200

    til page 133

--- Mobile/Controllers/HomeController.cs
+++ Mobile/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Mobile.Models;
 
 namespace Mobile.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private readonly Programmer[] _progs = 
+        {
+            new Programmer("Alice", "Smith", "Lead Developer", "Paris", "France", "C#"),
+            new Programmer("Joe", "Dunston", "Developer", "London", "UK", "Java"),
+            new Programmer("Peter", "Jones", "Developer", "Chicago", "USA", "C#"),
+            new Programmer("Murray", "Woods", "Jnr Developer", "Boston", "USA", "C#")
+        };
+
         public ActionResult Index()
         {
-            return View();
+            return View(_progs);
         }
     }
 }
