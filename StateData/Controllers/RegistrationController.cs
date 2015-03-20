8b8bc15f7b84fc2c68017716376f3ab36ff9ce03
Aviv Shafir <aviv@optimalfinance.co.il>
2015-03-20 12:55:04 +0200

    til page 236 - sessions

---
+++ StateData/Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StateData.Infrastructure;
+
+namespace StateData.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessFirstForm(string name)
+        {
+            SessionStateHelper.Set(SessionStateKeys.NAME, name);
+            return View("SecondForm");
+        }
+
+        [HttpPost]
+        public ActionResult CompleteForm(string country)
+        {
+            ViewBag.Name = SessionStateHelper.Get(SessionStateKeys.NAME);
+            ViewBag.Country = country;
+            return View();
+        }
+    }
+}
